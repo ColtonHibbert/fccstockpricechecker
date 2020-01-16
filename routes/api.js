39c12a4b714df3961d171862913ae423c441e2de
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2020-01-15 21:03:54 -0700

    working on http request on server, google is blocking express requests

--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,7 @@
 
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
+const fetch = require('node-fetch');
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
@@ -17,7 +18,14 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
+      console.log('hi')
+      console.log(req.query)
+      fetch('https://finance.google.com/finance/info?q=NASDAQ%3aGOOG').then(
+        res => res.json()
+      ).then(data => {
+        console.log(data)
+      })
+      const stockData = {};
     });
     
 };
