1bd58664655b1227e007ff1c3bf2744ed9bceb59
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2020-01-16 21:04:50 -0700

    working on api calls on server

--- routes/api.js
+++ routes/api.js
@@ -21,12 +21,31 @@ module.exports = function (app) {
       console.log(process.env.ALPHA_VANTAGE_KEY)
       console.log('hi')
       console.log(req.query)
-      fetch(`https://alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=${process.env.ALPHA_VANTAGE_KEY}`).then(
-        res => res.json()
-      ).then(data => {
-        console.log(data)
-      })
-      const stockData = {};
+      let stockSymbol1;
+      let stockSymbol2;
+      if(req.query.stock[1]) {
+        stockSymbol1 = req.query.stock[0];
+        stockSymbol2 = req.query.stock[1];
+      } 
+      if(req.query.stock[0] !== true) {
+        stockSymbol1 = req.query.stock
+      }
+      
+      function getStockPrice(symbol) {
+        fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&outputsize=compact&apikey=${process.env.ALPHA_VANTAGE_KEY}`).then(
+          res => res.json()
+        ).then(data => {
+          console.log(data)
+          return data;
+        })
+      }
+      const stockDataResponse = {};
+
+      if(stockSymbol1) {
+        stockData.stock = getStockPrice()
+      }
+      
+      
     });
     
 };
